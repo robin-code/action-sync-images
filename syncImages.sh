29c67a6645dd8ea6ea11469800d88737e4cf2a65
robin.li <robin-develop@outlook.com>
2025-06-13 22:16:42 +0800

    feat: sync images

--- syncImages.sh
+++ syncImages.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
+#export IMAGE_REGISTRY_NAME_SPACE=registry.cn-hangzhou.aliyuncs.com/robin-public
+#echo ${IMAGE_REGISTRY_NAME_SPACE}
 set -e
-
 # 检查变量是否已设置
 if [[ -z "${IMAGE_REGISTRY_NAME_SPACE}" ]]; then
   echo "❌ 环境变量 IMAGE_REGISTRY_NAME_SPACE 未设置。请设置为你的阿里云镜像命名空间（如 registry.cn-hangzhou.aliyuncs.com/your-namespace）"
@@ -15,10 +16,14 @@ while IFS= read -r image || [[ -n "$image" ]]; do
     continue
   fi
 
-  # 提取镜像名称和版本号
-  IMAGE_NAME=$(echo "$image" | awk -F'/' '{split($NF,a,":"); print a[1]}')
+  # 提取版本号
   VERSION_TAG=$(echo "$image" | awk -F: '{print $2}')
-  TARGET_IMAGE="${IMAGE_REGISTRY_NAME_SPACE}/${IMAGE_NAME}:${VERSION_TAG}"
+  echo "提取出image_tag=${VERSION_TAG}"
+  # 提取镜像仓库和路径，替换斜杠为下划线
+  IMAGE_PATH=$(echo "$image" | awk -F: '{print $1}' | sed 's/\//_/g')
+  echo "IMAGE_PATH=${IMAGE_PATH}"
+  echo ${IMAGE_REGISTRY_NAME_SPACE}
+  TARGET_IMAGE="${IMAGE_REGISTRY_NAME_SPACE}/${IMAGE_PATH}:${VERSION_TAG}"
   
   echo "🚀 同步镜像: $image -> $TARGET_IMAGE"
   skopeo copy docker://$image docker://$TARGET_IMAGE
